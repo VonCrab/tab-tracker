56f82bf97272e4006db5fc3817538bd75b57c481
VonCrab <lukepensiero@gmail.com>
2017-09-08 23:41:38 -0400

    set-up reg page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>Register</h1>
+
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email" />
+
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password" />
+
+    <button
+      @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/authentication_service'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
